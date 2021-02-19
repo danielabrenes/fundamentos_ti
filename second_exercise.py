d60e1597f628e3843ca7679ecd6cd73cea345714
Daniela <dabregue@gmail.com>
2021-02-19 12:12:07 -0600

    The second one is related to average of a student, third one is realted to a temperature

---
+++ second_exercise.py
@@ -0,0 +1,9 @@
+Nota1=0
+Nota2=0
+Nota3=0
+Promedio=0
+Nota1=float(input("Digite la nota 1: "))
+Nota2=float(input("Digite la nota 2: "))
+Nota3=float(input("Digite la nota 3: "))
+Promedio=(Nota1 + Nota2 + Nota3)/3
+print("El promedio es: ", Promedio)
---
+++ third_exersice.py
@@ -0,0 +1,5 @@
+tempc=0
+tempf=0
+tempc=float(input("Digite la temp en c:"))
+tempf=(tempc*1.8)+32
+print("La temp en F es:",tempf)
